2f15ae4da0bb80f842e50dd4336facf0738d6ca3
ghulamghousdev <itsghulamghous@gmail.com>
2020-07-05 18:37:58 +0500

    Implemented Clock replacement Algorithm

--- src/buffer/clock_replacer.cpp
+++ src/buffer/clock_replacer.cpp
@@ -10,20 +10,76 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <cstdio>
+#include <thread>  // NOLINT
+#include <vector>
+
 #include "buffer/clock_replacer.h"
 
 namespace bustub {
 
-ClockReplacer::ClockReplacer(size_t num_pages) {}
+ClockReplacer::ClockReplacer(size_t num_pages) {
+   using std::vector; 
+   for(size_t i = 0; i != num_pages; i++){
+    	replacer.emplace_back(-1);
+   }
+   framesInBuffer = num_pages;
+   clockHand = 0;
+   framesInReplacer = 0;
+}
 
 ClockReplacer::~ClockReplacer() = default;
 
-bool ClockReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool ClockReplacer::Victim(frame_id_t *frame_id) {
+ latch.lock();
+ 
+  if (framesInReplacer <= 0) {
+    latch.unlock();
+    return false;
+  }
+  
+  while (true) {
+    if ( replacer[clockHand] == -1) {
+      clockHand = (clockHand + 1) % framesInBuffer;
+      continue;
+    }
+    
+    if (replacer[clockHand] == 1) {
+      replacer[clockHand] = 0;
+      clockHand = (clockHand + 1) % framesInBuffer;
+      continue;
+    }
+    
+	
+    *frame_id = clockHand;
+     replacer[clockHand] = -1;
+     framesInReplacer--;
+    break;
+  }
+  
+  latch.unlock();
+  return true;
+}
 
-void ClockReplacer::Pin(frame_id_t frame_id) {}
+void ClockReplacer::Pin(frame_id_t frame_id) {
+  latch.lock();
+  if (replacer[frame_id] !=- 1) {
+    replacer[frame_id] = -1;
+    framesInReplacer--;
+  }
 
-void ClockReplacer::Unpin(frame_id_t frame_id) {}
+  latch.unlock();
+}
 
-size_t ClockReplacer::Size() { return 0; }
+void ClockReplacer::Unpin(frame_id_t frame_id) {
+  latch.lock();
+  if (replacer[frame_id] == -1) {
+  	replacer[frame_id] = 1;
+  	framesInReplacer++;
+  }
+  latch.unlock();
+}
+
+size_t ClockReplacer::Size() { return framesInReplacer; }
 
 }  // namespace bustub
