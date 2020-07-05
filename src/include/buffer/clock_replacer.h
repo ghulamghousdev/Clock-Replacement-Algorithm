134c1c7ba8fac6093e20b9838811b198e708e3f7
ghulamghousdev <itsghulamghous@gmail.com>
2020-07-05 18:38:18 +0500

    Implemented Clock replacement Algorithm

--- src/include/buffer/clock_replacer.h
+++ src/include/buffer/clock_replacer.h
@@ -1,15 +1,3 @@
-//===----------------------------------------------------------------------===//
-//
-//                         BusTub
-//
-// clock_replacer.h
-//
-// Identification: src/include/buffer/clock_replacer.h
-//
-// Copyright (c) 2015-2019, Carnegie Mellon University Database Group
-//
-//===----------------------------------------------------------------------===//
-
 #pragma once
 
 #include <list>
@@ -19,34 +7,40 @@
 #include "buffer/replacer.h"
 #include "common/config.h"
 
-namespace bustub {
 
-/**
- * ClockReplacer implements the clock replacement policy, which approximates the Least Recently Used policy.
- */
-class ClockReplacer : public Replacer {
- public:
-  /**
-   * Create a new ClockReplacer.
-   * @param num_pages the maximum number of pages the ClockReplacer will be required to store
-   */
-  explicit ClockReplacer(size_t num_pages);
+    namespace bustub {
 
   /**
-   * Destroys the ClockReplacer.
+   * ClockReplacer implements the clock replacement policy, which approximates the Least Recently Used policy.
    */
-  ~ClockReplacer() override;
+  class ClockReplacer : public Replacer {
+   public:
+    /**
+     * Create a new ClockReplacer.
+     * @param num_pages the maximum number of pages the ClockReplacer will be required to store
+     */
+    explicit ClockReplacer(size_t num_pages);
 
-  bool Victim(frame_id_t *frame_id) override;
+    /**
+     * Destroys the ClockReplacer.
+     */
+    ~ClockReplacer() override;
 
-  void Pin(frame_id_t frame_id) override;
+    bool Victim(frame_id_t *frame_id) override;
 
-  void Unpin(frame_id_t frame_id) override;
+    void Pin(frame_id_t frame_id) override;
 
-  size_t Size() override;
+    void Unpin(frame_id_t frame_id) override;
 
- private:
-  // TODO(student): implement me!
-};
+    size_t Size() override;
+
+   private:
+   
+	size_t clockHand;
+    size_t framesInReplacer;
+    size_t framesInBuffer;
+    std::vector<int> replacer;
+    std::mutex latch;
+  };
 
 }  // namespace bustub
