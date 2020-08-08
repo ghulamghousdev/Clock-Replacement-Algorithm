f4aa469612d9d31606ffdf4fb679e9004854ba82
ghulamghousdev <itsghulamghous@gmail.com>
2020-08-08 14:16:27 +0500

    👌 IMPROVE: final version

--- src/include/buffer/clock_replacer.h
+++ src/include/buffer/clock_replacer.h
@@ -1,34 +1,47 @@
 #pragma once
+
 #include <list>
 #include <mutex>  // NOLINT
-#include <vector>
+#include <unordered_map>
+
 #include "buffer/replacer.h"
 #include "common/config.h"
-    namespace bustub {
-  /**
-   * ClockReplacer implements the clock replacement policy, which approximates the Least Recently Used policy.
-   */
-  class ClockReplacer : public Replacer {
-   public:
-    /**
-     * Create a new ClockReplacer.
-     * @param num_pages the maximum number of pages the ClockReplacer will be required to store
-     */
-    explicit ClockReplacer(size_t num_pages);
-    /**
-     * Destroys the ClockReplacer.
-     */
-    ~ClockReplacer() override;
 
-    bool Victim(frame_id_t *frame_id) override;
-    void Pin(frame_id_t frame_id) override;
-    void Unpin(frame_id_t frame_id) override;
-    size_t Size() override;
-   private:
-	size_t clockHand;
-    size_t framesInReplacer;
-    size_t framesInBuffer;
-    std::vector<int> replacer;
-    std::mutex latch;
-  };
+namespace bustub {
+
+/**
+ * ClockReplacer implements the clock replacement policy, which approximates the Least Recently Used policy.
+ */
+class ClockReplacer : public Replacer {
+ public:
+  /**
+   * Create a new ClockReplacer.
+   * @param num_pages the maximum number of pages the ClockReplacer will be required to store
+   */
+  explicit ClockReplacer(size_t num_pages);
+
+  /**
+   * Destroys the ClockReplacer.
+   */
+  ~ClockReplacer() override;
+
+  bool Victim(frame_id_t *frame_id) override;
+
+  void Pin(frame_id_t frame_id) override;
+
+  void Unpin(frame_id_t frame_id) override;
+
+  size_t Size() override;
+
+ private:
+  // a non-thread-safe implementation for unpinning page
+  void PinImpl(frame_id_t frame_id);
+
+  std::mutex mux_;
+  std::list<frame_id_t> frames_;
+  std::list<frame_id_t>::iterator clock_hand_;
+  std::unordered_map<frame_id_t, bool> ref_flag_;
+  std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> pin_pos_;
+};
+
 }  // namespace bustub
